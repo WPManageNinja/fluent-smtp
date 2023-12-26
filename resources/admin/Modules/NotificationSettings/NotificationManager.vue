bfdf505741e8cfef9292dc5e24a4df76872a9d9b
Shahjahan Jewel <cep.jewel@gmail.com>
2023-12-26 19:02:50 +0600

    Slack Init

---
+++ resources/admin/Modules/NotificationSettings/NotificationManager.vue
@@ -0,0 +1,48 @@
+<template>
+    <div v-if="notification_settings.telegram_notify_status">
+        <template v-if="selectedDriver == 'telegram'">
+            <telegram-notification :notification_settings="notification_settings"/>
+        </template>
+        <template v-else-if="selectedDriver == 'slack'">
+            <slack-notification :notification_settings="notification_settings"/>
+        </template>
+        <template>
+            <h3>Configure Telegram Bot</h3>
+            <telegram-notification :notification_settings="notification_settings"/>
+            <hr style="margin: 20px 0;" />
+            <h3>Or configure Slack Bot</h3>
+            <slack-notification :notification_settings="notification_settings"/>
+        </template>
+    </div>
+</template>
+
+<script type="text/babel">
+import TelegramNotification from './_TelegramNotification.vue';
+import SlackNotification from './_SlackNotification.vue';
+
+export default {
+    name: 'NotificationManager',
+    components: {
+        TelegramNotification,
+        SlackNotification
+    },
+    props: {
+        notification_settings: {
+            type: Object,
+            required: true
+        }
+    },
+    computed: {
+        selectedDriver() {
+            if (this.notification_settings.telegram_notify_status == 'yes') {
+                return 'telegram';
+            }
+            if (this.notification_settings.slack_notify_status == 'yes') {
+                return 'slack';
+            }
+
+            return 'unknown';
+        }
+    }
+}
+</script>
